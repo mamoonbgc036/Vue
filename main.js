9abd46f3c6a833593b040f32d82d43830ca5002b
Abdullah Al Mamoon <mamoon036@gmail.com>
2023-04-23 08:57:43 +0600

    23.04.2023

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 const App = Vue.createApp({
-    data: function(){
-        return {
-            fruit: 'Apple',
-            image: './images/orange.jpg',
-            inventory : 15,
+    data(){
+        return{
+            cart:0,
+            product:'Apple',
+            brand:'Canada',
+            image:'./images/banana.jpg',
+            inStock: true,
+            details:['80% cotton','20% polyster'],
+            variants:[
+                {id:1,color:'green', image:'./images/orange.jpg', quantity:50},
+                {id:2,color:'red', image:'./images/Red_Apple.jpg', quantity:0}
+            ]
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variant){
+            this.image = variant.image
+            if(variant.quantity <= 0){
+                this.inStock = false
+            }
+        }
+    },
+    computed:{
+        title(){
+            return this.product+'  '+this.brand
         }
     }
 })
